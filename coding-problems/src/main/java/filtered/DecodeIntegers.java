93cc6c6143a3834d3f6394a8c5dda907fed7f7cf
chashi <>
2018-12-18 11:47:01 -0500

    refactor

--- coding-problems/src/main/java/filtered/DecodeIntegers.java
+++ coding-problems/src/main/java/filtered/DecodeIntegers.java
@@ -4,15 +4,20 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class DecodeIntegers {
-	public static void main(String[] args) throws Exception {
-		String input = "11234";
+	private static void writeToSystemIn(String input) throws UnsupportedEncodingException {
 		InputStream testInput = new ByteArrayInputStream(input.getBytes("UTF-8"));
 		System.setIn(testInput);
+	}
+
+	public static void main(String[] args) throws Exception {
+		String input = "11234";
+		writeToSystemIn(input);
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String encoded = br.readLine();
@@ -37,9 +42,9 @@ public class DecodeIntegers {
 		}
 
 		String firstNum = input.substring(0, 1);
-		String unencoded = unencodeNumber(firstNum);
+		String decoded = decodeNumber(firstNum);
 		String inputWithFirstDigitRemoved = input.substring(1);
-		permuteCombinations(inputWithFirstDigitRemoved, combination + unencoded, combinations);
+		permuteCombinations(inputWithFirstDigitRemoved, combination + decoded, combinations);
 
 		if (input.length() >= 2) {
 			String firstTwoNums = input.substring(0, 2);
@@ -47,13 +52,13 @@ public class DecodeIntegers {
 			if (firstTwo > 26) {
 				return;
 			}
-			unencoded = unencodeNumber(firstTwoNums);
+			decoded = decodeNumber(firstTwoNums);
 			String inputWithFirstTwoDigitsRemoved = input.substring(2);
-			permuteCombinations(inputWithFirstTwoDigitsRemoved, combination + unencoded, combinations);
+			permuteCombinations(inputWithFirstTwoDigitsRemoved, combination + decoded, combinations);
 		}
 	}
 
-	private static String unencodeNumber(String number) {
+	private static String decodeNumber(String number) {
 		int num = Integer.parseInt(number);
 		num = num + 96;
 		char mapped = (char) num;
